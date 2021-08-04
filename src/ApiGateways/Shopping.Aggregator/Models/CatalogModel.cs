2aed904a73877cd914118bdc6bedf59e60a806d8
Syed Farooq Ahmed <sfarooq.ahmed@hyperdatacomputing.com>
2021-08-04 14:50:12 +0500

    --> Shopping Aggregator Created

---
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class CatalogModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summery { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
